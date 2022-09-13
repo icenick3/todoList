c250e8cb3d5558e69d6ca42bb9cb845a3b2326eb
kolya <kolyapadda@gmail.com>
2022-09-14 01:43:04 +0300

    save5

--- src/components/Forms/FormForLogin/FormForLogin.js
+++ src/components/Forms/FormForLogin/FormForLogin.js
@@ -1,12 +1,12 @@
 import React, {useState} from 'react';
-import {getAuth, signInWithEmailAndPassword} from "firebase/auth";
+import {createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword} from "firebase/auth";
 import {useForm} from "react-hook-form";
 import {useDispatch} from "react-redux";
 
 import './FormForLogin.css'
 import {setUser} from "../../../store/slices/userSlice";
 
-const FormForLogin = ({setLogin}) => {
+const FormForLogin = ({setActive}) => {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -14,7 +14,29 @@ const FormForLogin = ({setLogin}) => {
     const {handleSubmit, reset} = useForm();
     const dispatch = useDispatch()
 
-    const onSubmit = () => {
+
+    const onSubmitForRegistration = () => {
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                const user = userCredential.user;
+                console.log(user)
+                dispatch(setUser({
+                    email: user.email,
+                    id: user.uid,
+                    token: user.accessToken
+                }))
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorCode)
+                console.log(errorMessage)
+            });
+        reset()
+    }
+
+
+    const onSubmitForLogin = () => {
         signInWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
                 const user = userCredential.user;
@@ -35,13 +57,26 @@ const FormForLogin = ({setLogin}) => {
 
     return (
         <div>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <input type="email"  onChange={(e) => setEmail(e.target.value)}/>
-                <input type="password"  onChange={(e) => setPassword(e.target.value)}/>
-                <button>Login</button>
-                or
-                <button onClick={() => {setLogin(false)}}>Registration</button>
-            </form>
+            <div className="main">
+                <input className={'input'} type="checkbox" id="chk" aria-hidden="true"/>
+                    <div className="signup">
+                        <form onSubmit={handleSubmit(onSubmitForRegistration)}>
+                            <label className={'label'} htmlFor="chk" aria-hidden="true">Sign up</label>
+                                <input className={'input'} type="email" name="email" placeholder="Email" required="" onChange={(e) => setEmail(e.target.value)}/>
+                                    <input className={'input'} type="password" name="pswd" placeholder="Password" required="" onChange={(e) => setPassword(e.target.value)}/>
+                                        <button className={'button'} onClick={()=>setActive(false)}>Sign up</button>
+                        </form>
+                    </div>
+
+                    <div className="login">
+                        <form onSubmit={handleSubmit(onSubmitForLogin)}>
+                            <label className={'label'} htmlFor="chk" aria-hidden="true">Login</label>
+                            <input className={'input'} type="email" name="email" placeholder="Email" required="" onChange={(e) => setEmail(e.target.value)}/>
+                                <input className={'input'} type="password" name="pswd" placeholder="Password" required="" onChange={(e) => setPassword(e.target.value)}/>
+                                    <button className={'button'} onClick={()=>setActive(false)}>Login</button>
+                        </form>
+                    </div>
+            </div>
         </div>
     );
 };
--- src/components/ModalWindows/ModalWindowForRegistration/ModalWindowForRegistration.js
+++ src/components/ModalWindows/ModalWindowForRegistration/ModalWindowForRegistration.js
@@ -2,22 +2,17 @@ import React, {useState} from 'react';
 
 import './ModalWindowForRegistration.css'
 import {FormForLogin}from "../../Forms/FormForLogin/FormForLogin";
-import {FormForRegistration} from "../../Forms/FormForRegistration/FormForRegistration";
 
 const ModalWindowForRegistration = ({active, setActive}) => {
 
-    const [login, setLogin] = useState(true)
-
     const onClick = () => {
         setActive(false)
-        setLogin(true)
     }
 
     return (
         <div className={active ? "modal active" : "modal"} onClick={onClick}>
             <div className={active ? "modal_content active" : "modal_content"} onClick={e => e.stopPropagation()}>
-                {login && <FormForLogin setLogin={setLogin}/>}
-                {!login && <FormForRegistration/>}
+                <FormForLogin setActive={setActive}/>
             </div>
 
         </div>
